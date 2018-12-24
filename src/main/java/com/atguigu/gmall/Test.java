9bcb7704a098da32421d152f2b934d4fa8e53299
Thexiu <44971938+Thexiu@users.noreply.github.com>
2018-12-24 11:50:51 +0800

    Update Test.java

--- src/main/java/com/atguigu/gmall/Test.java
+++ src/main/java/com/atguigu/gmall/Test.java
@@ -5,5 +5,6 @@ public class Test {
         System.out.println("第一次提交");
          System.out.println("第一次克隆");
         System.out.println("第二次提交");
+        System.out.println("第二次clone");
     }
 }
